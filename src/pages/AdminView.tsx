29b26c0229e9141bfbad102b7ccffb2fbc0e7a7a
Alexa Meza <alexapuch@hotmail.com>
2026-06-20 17:07:58 -0500

    bump: version v2.26

--- src/pages/AdminView.tsx
+++ src/pages/AdminView.tsx
@@ -38,7 +38,7 @@ const CroquisRiesgosModal = lazy(() => import('../components/admin/CroquisRiesgo
 import { generateDC3PDF } from '../utils/generateDC3PDF';
 import { Menu } from 'lucide-react';
 
-const APP_VERSION = 'v2.25';
+const APP_VERSION = 'v2.26';
 const SESSION_KEY = 'adminAuth';
 const SESSION_VERSION_KEY = 'adminAuthVersion';
 
